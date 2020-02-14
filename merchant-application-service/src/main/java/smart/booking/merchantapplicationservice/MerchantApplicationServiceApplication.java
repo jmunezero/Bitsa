a80442f40fbe9a5f63823f3b9ca2c0d25e11b313
jmunezero <jsmunezero@gmail.com>
2020-02-13 23:39:35 -0600

    Create client-registration-service and merchant-application-service

---
+++ merchant-application-service/src/main/java/smart/booking/merchantapplicationservice/MerchantApplicationServiceApplication.java
@@ -0,0 +1,13 @@
+package smart.booking.merchantapplicationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MerchantApplicationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MerchantApplicationServiceApplication.class, args);
+    }
+
+}
---
+++ merchant-application-service/src/test/java/smart/booking/merchantapplicationservice/MerchantApplicationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package smart.booking.merchantapplicationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MerchantApplicationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
